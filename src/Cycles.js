e1b6b378ed231093575aab0519a182dc303eb719
Наталья <natasha291700@gmail.com>
2022-05-07 13:10:51 +0300

    task-cycles_1

--- src/Cycles.js
+++ src/Cycles.js
@@ -5,7 +5,7 @@
 */
 export function rangeSum(start, end) {
     let sum = 0;
-    for (let i = start; (i = end); i += 1) {
+    for (let i = start; i <= end; i += 1) {
         if (i % 2 == 0) {
             sum += i;
         }
@@ -20,7 +20,7 @@ export function rangeSum(start, end) {
 */
 export function iterationCount(a) {
     let iteration = 0;
-    while ((a > 0, 1)) {
+    while (a > 0.1) {
         a /= 2;
         iteration += 1;
     }
